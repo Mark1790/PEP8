9be1186978ad6ff8f624911633cb62aa30da4e48
Mark1790 <126524237+Mark1790@users.noreply.github.com>
2023-05-02 19:45:16 -0700

    Add files via upload

---
+++ pep8.py
@@ -0,0 +1,9 @@
+class User():
+    def __init__(self, username, password=""):
+self.username = username
+self.password = password
+
+def set_password(self, password):
+        self.password = password
+        
+cody_user = User('Cody')
